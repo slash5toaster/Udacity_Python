8bbc6336b964d0888fa96d914c7567292ff44795
Clyde Jones <jones.clyde@gene.com>
2017-09-09 16:09:44 -0700

    Project 1

---
+++ Breaktime/breaktime.py
@@ -0,0 +1,13 @@
+import webbrowser
+import time
+
+timer_count = 3
+break_count = 0
+
+print ("Program started on " + time.ctime())
+while break_count < timer_count:
+    # print ("on break " + break_count)
+    time.sleep(10)
+    webbrowser.open("https://youtu.be/dOlrYz9cJMI")
+    break_count = break_count +1
+    
