1339ef9927ae7c7563945c268c79e0f300103ee7
Clyde Jones <jones.clyde@gene.com>
2017-09-10 09:38:24 -0700

    Added loop to build list of files printing files

--- Secret_Message/decode.py
+++ Secret_Message/decode.py
@@ -1,14 +1,18 @@
 import os
+from os import listdir
+from os.path import isfile, join
+
+#Our plan
 # 1. Get filenames
 # 2. for each filename
 #    2.1 calculate updated filename
 #    2.2 rename file to updated filename
-#    2.3
 
-from os import walk
+mypath = "/Users/jonesc47/Google Drive/Python/PythonClass_Udacity/Secret_Message/prank"
 
-f = []
-for (dirpath, dirnames, filenames) in walk("/Users/jonesc47/Google Drive/Python/PythonClass_Udacity/Secret_Message/prank"):
-    f.extend(filenames)
-    break
-# print(f.filenames)
+#get all the files in the directory
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+# act on each file
+for fle in onlyfiles:
+    print fle
