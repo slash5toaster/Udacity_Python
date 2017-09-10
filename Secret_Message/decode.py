69c468b6a97117d857bd3ddbc196188edc6362a6
Clyde Jones <jones.clyde@gene.com>
2017-09-10 08:15:25 -0700

    directory walk

--- Secret_Message/decode.py
+++ Secret_Message/decode.py
@@ -1,1 +1,14 @@
 import os
+# 1. Get filenames
+# 2. for each filename
+#    2.1 calculate updated filename
+#    2.2 rename file to updated filename
+#    2.3
+
+from os import walk
+
+f = []
+for (dirpath, dirnames, filenames) in walk("/Users/jonesc47/Google Drive/Python/PythonClass_Udacity/Secret_Message/prank"):
+    f.extend(filenames)
+    break
+# print(f.filenames)
