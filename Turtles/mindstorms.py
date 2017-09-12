d6074e6473cc967852108ced591ebc37a9d14100
Clyde Jones <jones.clyde@gene.com>
2017-09-11 17:01:40 -0700

    first pass at making shapes

---
+++ Turtles/mindstorms.py
@@ -0,0 +1,13 @@
+import turtle
+
+def draw_square()
+    #get mah window
+    window = turtle.Screen()
+    window.bgcolor("red")
+
+    chuck = turtle.Turtle()
+    chuck.forward(100)
+
+    window.exitonclick()
+
+draw_square()
