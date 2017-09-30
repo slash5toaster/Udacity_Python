1eb4d7189ec5a33f42a8d5a97c552df301929e94
Clyde Jones <jones.clyde@gene.com>
2017-09-30 10:40:36 -0700

    Initial runn for movie class

---
+++ Movie/entertainment_center.py
@@ -0,0 +1,7 @@
+import media
+
+toystory = media.Movie("Toy Story",
+                       "A story of a boy's toys that come to life",
+                       "https://en.wikipedia.org/wiki/File:Toy_Story.jpg#/media/File:Toy_Story.jpg",
+                       "https://www.youtube.com/watch?v=4KPTXpQehio")
+print(toystory.storyline)
