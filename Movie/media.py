82c1d4b401a6f8ec1a767fcdc8398165a58f13de
Clyde Jones <jones.clyde@gene.com>
2017-09-30 16:26:23 -0700

    removed youtube refs

--- Movie/media.py
+++ Movie/media.py
@@ -1,11 +1,11 @@
 import webbrowser
 
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_video):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+        self.trailer_video_url = trailer_video
 
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        webbrowser.open(self.trailer_video_url)
