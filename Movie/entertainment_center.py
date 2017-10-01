088445c48378f9b4189a233a4d38c21d0d65670d
Clyde Jones <jones.clyde@gene.com>
2017-10-01 10:04:57 -0700

    fixed show trailer

--- Movie/entertainment_center.py
+++ Movie/entertainment_center.py
@@ -4,9 +4,11 @@ toystory = media.Movie("Toy Story",
                        "A story of a boy's toys that come to life",
                        "https://en.wikipedia.org/wiki/File:Toy_Story.jpg#/media/File:Toy_Story.jpg",
                        "https://www.youtube.com/watch?v=4KPTXpQehio")
-print(toystory.storyline)
+# print(toystory.storyline)
 
 avatar = media.Movie("Avatar: The Last Airbender",
                      "A magical teenager",
                      "https://images-na.ssl-images-amazon.com/images/M/MV5BZTdmY2YxYTctMTY0Mi00YmRhLWEyNzEtZDJiOTQyYTFkODVkXkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_.jpg",
                      "http://www.imdb.com/title/tt0417299/videoplayer/vi2716336409")
+print(avatar.storyline)
+avatar.show_trailer()
