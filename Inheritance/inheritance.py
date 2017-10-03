23d28b472bec2720047a11112aa9801a5081930f
Clyde Jones <jones.clyde@gene.com>
2017-10-02 18:01:30 -0700

    functions

--- Inheritance/inheritance.py
+++ Inheritance/inheritance.py
@@ -5,6 +5,10 @@ class Parent():
         print("Parent Constructor Called")
         self.last_name = last_name
         self.eye_color = eye_color
+    def show_info(self):
+         print("Last Name - "+self.last_name)
+         print("Eye Color - "+self.eye_color)
+
 
 class Child(Parent):
     """Class for Child, inherited from Parent"""
@@ -21,5 +25,6 @@ class Child(Parent):
 # billy_cyrus = Parent("Cyrus", "blue")
 # print (billy_cyrus.last_name)
 miley_cyrus = Child("Cyrus", "blue", 5)
-print (miley_cyrus.last_name)
+miley_cyrus.show_info()
+# print (miley_cyrus.last_name)
 print (miley_cyrus.number_of_toys)
